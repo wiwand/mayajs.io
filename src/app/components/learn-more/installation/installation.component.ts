ffa068feaed7ca8a5493967d9a9fd2df21bf8811
Riland <rlndbrbd@gmail.com>
2020-04-18 02:06:00 +0800

    FEAT: Update installation component

--- src/app/components/learn-more/installation/installation.component.ts
+++ src/app/components/learn-more/installation/installation.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./installation.component.scss"],
 })
 export class InstallationComponent implements OnInit {
+
+  svg = {
+    copy: "../assets/svg/copypaste-icon.svg",
+    installation: "../assets/svg/Installation.svg"
+  };
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    document.getElementById('btn-id').addEventListener("click", ()=>{
+      var clipBoard = document.createElement("input");
+      document.body.appendChild(clipBoard);
+      clipBoard.value = document.getElementById('copyme').innerHTML;
+      clipBoard.select();
+      document.execCommand('copy');
+      document.body.removeChild(clipBoard);
+      });
+    }
+
 }
